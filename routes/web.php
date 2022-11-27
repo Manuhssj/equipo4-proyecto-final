5c9eef26d381c5cf05bf83c87a7ab5e020fe83c6
Diego Ochoa <you@example.com>
2022-11-26 17:44:32 -0700

    Se evita la navegacion si no estas loggeado

--- routes/web.php
+++ routes/web.php
@@ -22,80 +22,81 @@ use App\Http\Controllers\ClientController;
 
 
 
-Route::get('/', function () {
-    /* return User::all(); */
-    return view('welcome');
-});
+    Route::get('/', function () {
+        /* return User::all(); */
+        return view('welcome');
+    });
 
-Route::get('/login', function () {
-    return redirect('/');
-})->name('login');
+    Route::get('/login', function () {
+        return redirect('/');
+    })->name('login');
 
 
 
-/* Route::get('/users', function () {
-    return view('users.index');
-})->name('users.index'); */
+    /* Route::get('/users', function () {
+        return view('users.index');
+    })->name('users.index'); */
 
-Route::get('users/', [UserController::class, 'index'])->name('users.index');
-Route::get('users/{id}/', [UserController::class, 'show'])->name('users.detailUser');
-Route::post('users/', [UserController::class, 'store'])->name('users.store');
-Route::put('users/{id}/', [UserController::class, 'update'])->name('users.update');
-Route::delete('users/{id}/', [UserController::class, 'delete'])->name('users.delete');
+    Route::get('users/', [UserController::class, 'index'])->middleware(['auth', 'verified'])->name('users.index');
+    Route::get('users/{id}/', [UserController::class, 'show'])->middleware(['auth', 'verified'])->name('users.detailUser');
+    Route::post('users/', [UserController::class, 'store'])->middleware(['auth', 'verified'])->name('users.store');
+    Route::put('users/{id}/', [UserController::class, 'update'])->middleware(['auth', 'verified'])->name('users.update');
+    Route::delete('users/{id}/', [UserController::class, 'delete'])->middleware(['auth', 'verified'])->name('users.delete');
 
-Route::get('clients/', [ClientController::class, 'index'])->name('clients.index');
-Route::get('clients/{id}/', [ClientController::class, 'show'])->name('clients.detailClient');
-Route::post('clients/', [ClientController::class, 'store'])->name('clients.store');
-Route::put('clients/{id}/', [ClientController::class, 'update'])->name('clients.update');
-Route::delete('clients/{id}/', [ClientController::class, 'delete'])->name('clients.delete');
+    Route::get('clients/', [ClientController::class, 'index'])->middleware(['auth', 'verified'])->name('clients.index');
+    Route::get('clients/{id}/', [ClientController::class, 'show'])->middleware(['auth', 'verified'])->name('clients.detailClient');
+    Route::post('clients/', [ClientController::class, 'store'])->middleware(['auth', 'verified'])->name('clients.store');
+    Route::put('clients/{id}/', [ClientController::class, 'update'])->middleware(['auth', 'verified'])->name('clients.update');
+    Route::delete('clients/{id}/', [ClientController::class, 'delete'])->middleware(['auth', 'verified'])->name('clients.delete');
 
-Route::get('/users/detail-user', function () {
-    return view('users.detailUser');
-})->name('users.detailUser');
+    Route::get('/users/detail-user', function () {
+        return view('users.detailUser');
+    })->middleware(['auth', 'verified'])->name('users.detailUser');
 
-Route::get('/activities', function () {
-    return view('categories.activities');
-})->name('categories.activities');
+    Route::get('/activities', function () {
+        return view('categories.activities');
+    })->middleware(['auth', 'verified'])->name('categories.activities');
 
-Route::get('/tours', function () {
-    return view('categories.tours');
-})->name('categories.tours');
+    Route::get('/tours', function () {
+        return view('categories.tours');
+    })->middleware(['auth', 'verified'])->name('categories.tours');
 
-Route::get('/private-tours', function () {
-    return view('categories.privatesTours');
-})->name('categories.privasteTours');
+    Route::get('/private-tours', function () {
+        return view('categories.privatesTours');
+    })->middleware(['auth', 'verified'])->name('categories.privasteTours');
 
-/* Route::get('/clients', function () {
-    return view('clients.index');
-})->name('clients.index');
-Route::get('/clients/detail-client', function () {
-    return view('clients.detailClient');
-})->name('clients.detailClient'); */
+    /* Route::get('/clients', function () {
+        return view('clients.index');
+    })->name('clients.index');
+    Route::get('/clients/detail-client', function () {
+        return view('clients.detailClient');
+    })->name('clients.detailClient'); */
 
-Route::get('/services', function () {
-    return view('services.index');
-})->name('services.index');
+    Route::get('/services', function () {
+        return view('services.index');
+    })->middleware(['auth', 'verified'])->name('services.index');
 
-Route::get('/services/detail-service', function () {
-    return view('services.detailService');
-})->name('services.detailService');
+    Route::get('/services/detail-service', function () {
+        return view('services.detailService');
+    })->middleware(['auth', 'verified'])->name('services.detailService');
 
-Route::get('/reservations', function () {
-    return view('reservations.index');
-})->name('reservations.index');
+    Route::get('/reservations', function () {
+        return view('reservations.index');
+    })->middleware(['auth', 'verified'])->name('reservations.index');
 
-Route::get('/reservations/detail-reservation', function () {
-    return view('reservations.detailReservation');
-})->name('reservations.detailReservation');
+    Route::get('/reservations/detail-reservation', function () {
+        return view('reservations.detailReservation');
+    })->middleware(['auth', 'verified'])->name('reservations.detailReservation');
 
-Route::get('/dashboard', function () {
-    return view('dashboard.index');
-})->name('dashboard.index');
+    Route::get('/dashboard', function () {
+        return view('dashboard.index');
+    })->middleware(['auth', 'verified'])->name('dashboard.index');
 
-Route::get('/reports', function () {
-    return view('reports.index');
-})->name('reports.index');
+    Route::get('/reports', function () {
+        return view('reports.index');
+    })->middleware(['auth', 'verified'])->name('reports.index');
 
-Route::get('/reports/detail-report', function () {
-    return view('reports.detailReport');
-})->name('reports.detailReport');
+    Route::get('/reports/detail-report', function () {
+        return view('reports.detailReport');
+    })->middleware(['auth', 'verified'])->name('reports.detailReport');
+
