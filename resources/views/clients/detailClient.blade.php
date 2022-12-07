60c04bfc4f1d8bd41c4f0ea6323f789755bf943c
Diego Ochoa <you@example.com>
2022-12-06 20:12:32 -0700

    Redireccionamiento vista clientes

--- resources/views/clients/detailClient.blade.php
+++ resources/views/clients/detailClient.blade.php
@@ -200,7 +200,7 @@
 
                                 <td>
                                     <div class="hstack gap-3 fs-15">
-                                        <!-- <a href="javascript:void(0);" class="link-primary"></a> -->
+                                    <a href="{{ route('services.detailService') }} " class="link-primary">
                                         <button type="button" class="btn btn-primary">
                                             <i class="ri-eye-line"></i>
                                         </button>
